6eb83c2ab761df451aa61594c68383ea0320988d
jeffdaly1975 <daly.jeff@gmail.com>
2026-02-20 13:49:54 -0500

    added files

--- kolam_worker_getKolamData.sh
+++ kolam_worker_getKolamData.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 #
-# 2026-02-14 Feb.Sat 11:04:23 Worker script to make eth_call to KOLAM to get information on a Kolam by TokenId
+# 2026-02-13 Feb.Fri 11:43:47 Worker script to make eth_call to KOLAM to get information on a Kolam by TokenId
 #
 
 # USAGE:
-# kolam_worker_tokenURI.sh 98328815286472208108725841938299315763074621854053660676912324279791346200393
+# kolam_worker_getKolamData.sh 98328815286472208108725841938299315763074621854053660676912324279791346200393
 
 # Contract name:          ? Kolam implementation?
 # Contract address:       0xbFcdEB33cf38eB8b0b5A201b6c4b5b1d8329b5D3
 # Implementation address: 
 
-# Call function:          tokenURI()
-# function hash:          0xc87b56dd
+# Call function:          getKolamData()
+# function hash:          0x7a6eddfe
 
 # Check if .env file exists
 if [ -f .env ]; then
@@ -37,10 +37,16 @@ kTokenId=$1
 
 
 
+# OUTPUT LOOKS LIKE THIS:
+### Lisas-MacBook-Pro-2:otom lisadaly$ ./tempmineables.alchemy.sh 
+### [{"jsonrpc":"2.0","id":1,"result":"0x0000000000000000000000000000000000000000000000000000000000000015"}
+### ,{"jsonrpc":"2.0","id":2,"result":"0x000000000000000000000000000000000000000000000000000000000000001c"}
+### ...
+
 curl --request POST  \
      --url https://shape-mainnet.g.alchemy.com/v2/$ALCHEMY_API_KEY  \
      --header 'accept: application/json'  \
      --header 'content-type: application/json' \
-     --data ' [ { "id":  1, "jsonrpc": "2.0", "method": "eth_call", "params": [ { "from": "0x620051B8553a724b742ae6ae9cC3585d29F49848", "to":   "0xbFcdEB33cf38eB8b0b5A201b6c4b5b1d8329b5D3", "data": "0xc87b56dd'$kTokenId'" }, "latest" ] }  ] '
+     --data ' [ { "id":  1, "jsonrpc": "2.0", "method": "eth_call", "params": [ { "from": "0x620051B8553a724b742ae6ae9cC3585d29F49848", "to":   "0xbFcdEB33cf38eB8b0b5A201b6c4b5b1d8329b5D3", "data": "0x7a6eddfe'$kTokenId'" }, "latest" ] }  ] '
 # example calldata copied from shapescan                                                                                                                                                0x7a6eddfed964232080ffa0a2a811860141895352cc3c8bced00293ad640b6b5759c72f49
 
